be3ccb4d9fed31e4d3de65492351b1020dbec590
RiteshJadhav021 <riteshjadhav3435@gmail.com>
2025-11-15 12:39:09 +0530

    ES MODULE learning

---
+++ ESModule/app.js
@@ -0,0 +1,18 @@
+// global.console.log("hello guys");
+
+// // globalThis.console.log(process);
+// globalThis.console.log(module);
+
+// const add=require('./math');
+
+// const {add,sub,div,mul}=require('./math');
+
+// import mul from './math.js';
+// import {mul,div} from './math.js';
+
+import {add,sub,div} from './math.js';
+
+console.log(add(5,10));
+console.log(sub(5,10));
+console.log(div(5,10));
+// console.log(mul(50,10));
---
+++ ESModule/math.js
@@ -0,0 +1,21 @@
+const add=(a,b)=>{
+    return a+b;
+}
+
+ const sub=(a,b)=>{
+    return a-b;
+}
+
+ const div=(a,b)=>{
+    return a/b;
+}
+
+// export const mul=(a,b)=>{
+//     return a*b;
+// }
+
+// module.exports={add,sub,div,mul};
+
+// export default mul;
+
+export {add,sub,div};
